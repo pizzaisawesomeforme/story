76cc2ba727c1fd8a20bccf0aacef2e242171843a
Gustavo Cruz <pizzaisawesomeforme@gmail.com>
2015-09-14 20:16:27 -0400

    save

---
+++ story/intro.py
@@ -0,0 +1,16 @@
+"""This is Bob's Journey
+
+    "Huh...where am I?" "Who am I?" You are Bob and you have been stranded by
+your family for your own safty.  Umm you don't want to know why... Lets
+just let Bob go on his Journy. "Well I'v got to get a home, hmmm oh I
+know I'll go find a village or I can just build my house just in case
+I waste my time finding a village." "Hmm... what should I do?"
+
+
+"""
+
+on_build="house"
+on_find="village"
+
+
+
---
+++ story/village.py
@@ -0,0 +1,4 @@
+"""You Have Gone to Find a Village
+
+Bob has chosen to go find a village and out of nowhere you find a bunch of townsfolk building a village and you go over to the village.
+"Can I help?" "Sure!" says one of the townsfolk. 
